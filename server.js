e577ca11dc098e6f2d48b0ab425deebe315b14ec
Oceane Andreis <ocan3794@colorado.edu>
2020-11-09 14:17:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ console.log("server is starting");
 var express = require('express');
 var app = express();
 var server = app.listen(3000, listening);
-//Tried this did not work
-// var firebase = require('firebase/app');
-// require('firebase/auth');
-// require('firebase/database');
 function listening(){  //callback
   console.log("listening...");
 }
