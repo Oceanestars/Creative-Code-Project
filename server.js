ac53691b60d1105339894116f74333ad63fdd476
Oceane Andreis <oceanestars13@gmail.com>
2020-11-24 01:07:10 -0700

    Fixed curly bracket

--- server.js
+++ server.js
@@ -5,5 +5,4 @@ const app = express(); // Init for express
 const port = process.env.PORT || 3000
 const server = http.createServer(app); // Creation of an HTTP server
 server.listen(port, () => console.log(`Server started on port ${port}...`))
-}
 app.use(express.static('website')); //host static files like img
